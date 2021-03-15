038ec30e37e08906f4c407df12c3db41a6a7cddb
Javi <inbox.javiergm@gmail.com>
2021-03-15 13:14:47 +0100

    App para loguear e instalación de un paquete

--- src/accexible/Site.js
+++ src/accexible/Site.js
@@ -1,12 +1,14 @@
 import Header from './Header'
 import Main from './Main'
 import Footer from './Footer'
+import Login from './users/Login'
 
 function Site() {
     return (
         <>
             <Header />
             <Main />
+            <Login />
             <Footer />
         </>
     )
