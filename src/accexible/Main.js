2015a7e49421a41ecabed62d17b851e403598f84
Santoxjon <jonsantox@gmail.com>
2021-03-15 16:38:39 +0100

    Refactor react router dom

--- src/accexible/Main.js
+++ src/accexible/Main.js
@@ -1,20 +1,18 @@
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Login from './users/Login';
 
 function Main() {
     return (
         <main>
-            <BrowserRouter>
-                <Route exact path="/">
-                    <p>Main</p>
-                </Route>
-                <Route exact path="/login">
-                    <Login />
-                </Route>
-                {/* <Route exact path="/register">
+            <Route exact path="/">
+                <p>Main</p>
+            </Route>
+            <Route exact path="/login">
+                <Login />
+            </Route>
+            {/* <Route exact path="/register">
                     <Register />
                 </Route> */}
-            </BrowserRouter>
         </main>
     )
-}export default Main;
+} export default Main;
