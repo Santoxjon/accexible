1335a046e238887f5a235df2b5e33deed2f92800
Javi <inbox.javiergm@gmail.com>
2021-03-16 19:13:55 +0100

    añadir required en userdetails

--- src/accexible/users/UserDetails.js
+++ src/accexible/users/UserDetails.js
@@ -6,49 +6,49 @@ import { Redirect } from 'react-router-dom';
 
 function UserDetails() {
 
-    let [userCookie, setUserCookie] = useState({ userId: getCookie("userId"), loginToken: getCookie("loginToken") });
-    let [usernameEdit, setUsernameEdit] = useState("");
-    let [userEmailEdit, setUserEmailEdit] = useState("");
-    let [usernameChange, setUsernameChange] = useState("");
-    let [userEmailChange, setUserEmailChange] = useState("");
-    let [userIdEdit, setUserIdEdit] = useState("");
-    let [userPasswordEdit, setUserPasswordEdit] = useState("");
-    let [userPasswordChange, setUserPasswordChange] = useState("");
-    
-function readUsernameEdit(event) {
-    setUsernameChange(event.target.value)
-}
+    const [userCookie, setUserCookie] = useState({ userId: getCookie("userId"), loginToken: getCookie("loginToken") });
+    const [usernameEdit, setUsernameEdit] = useState("");
+    const [userEmailEdit, setUserEmailEdit] = useState("");
+    const [usernameChange, setUsernameChange] = useState("");
+    const [userEmailChange, setUserEmailChange] = useState("");
+    const [userIdEdit, setUserIdEdit] = useState("");
+    const [userPasswordEdit, setUserPasswordEdit] = useState("");
+    const [userPasswordChange, setUserPasswordChange] = useState("");
 
-function readUserEmailEdit(event) {
-    setUserEmailChange(event.target.value)
-}
-
-function readUserPasswordEdit(event) {
-    setUserPasswordChange(event.target.value)
-}
-
-function editUser(e) {
-    e.preventDefault();
-    console.log(usernameChange, userEmailChange)
-    var editOldUser = {
-        name: usernameChange,
-        email: userEmailChange,
-        password: userPasswordChange,
-        id: userIdEdit
-    }
-
-    var fetchOldUser = {
-        method: 'PUT',
-        body: JSON.stringify(editOldUser),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8'
+    function readUsernameEdit(event) {
+            setUsernameChange(event.target.value)      
         }
+
+    function readUserEmailEdit(event) {       
+            setUserEmailChange(event.target.value)
     }
 
-    fetch(`http://localhost:9000/users/updateuser?userId=${userCookie.userId}&loginToken=${userCookie.loginToken}`, fetchOldUser)
-    .then(respuesta => respuesta.json())
-    .then((data => console.log(data)))
-}
+    function readUserPasswordEdit(event) {
+        setUserPasswordChange(event.target.value)
+    }
+
+    function editUser(e) {
+        e.preventDefault();
+        console.log(usernameChange, userEmailChange)
+        var editOldUser = {
+            name: usernameChange,
+            email: userEmailChange,
+            password: userPasswordChange,
+            id: userIdEdit
+        }
+
+        var fetchOldUser = {
+            method: 'PUT',
+            body: JSON.stringify(editOldUser),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            }
+        }
+
+        fetch(`http://localhost:9000/users/updateuser?userId=${userCookie.userId}&loginToken=${userCookie.loginToken}`, fetchOldUser)
+            .then(respuesta => respuesta.json())
+            .then((data => console.log(data)))
+    }
 
     useEffect(() => {
         console.log(userCookie)
@@ -69,27 +69,27 @@ function editUser(e) {
         let regExCapital = new RegExp(/[A-Z]/, 'g') //must contain 1 character from A-Z
 
         if ((userPasswordEdit.length > 7 && userPasswordEdit.length < 21) && regExNumber.test(userPasswordEdit) && regExCapital.test(userPasswordEdit)) {
-            
+
         }
         else {
-           
+
             if (!(userPasswordEdit.length > 7 && userPasswordEdit.length < 21)) {
-                
+
             }
             else {
-                
+
             }
             if (!regExNumber.test(userPasswordEdit)) {
-              
+
             }
             else {
-              
+
             }
             if (!regExCapital.test(userPasswordEdit)) {
-               
+
             }
             else {
-                
+
             }
         }
     }, [userPasswordEdit])
@@ -98,21 +98,23 @@ function editUser(e) {
 
         return (
             <>
-            <Form id="updateForm" onSubmit={editUser}>
-                <Form.Group>
-                    <Form.Label>Nombre y apellidos</Form.Label>
-                    <Form.Control type="text" placeholder={usernameEdit} id="inputname" name="name" onChange={readUsernameEdit}/>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder={userEmailEdit} id="inputemail" name="email" onChange={readUserEmailEdit} />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Contraseña</Form.Label>
-                    <Form.Control type="password" id="inputpassword" name="password" onChange={readUserPasswordEdit} />
-                </Form.Group>
-                <Button type="submit" >
-                    Enviar
+                <Form id="updateForm" onSubmit={editUser}>
+                    <Form.Group>
+                        <Form.Label>Nombre y apellidos</Form.Label>
+                        <Form.Control required type="text" placeholder={usernameEdit} id="inputname" name="name" onChange={readUsernameEdit} />
+                      
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control required type="email" placeholder={userEmailEdit} id="inputemail" name="email" onChange={readUserEmailEdit} />
+                       
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Contraseña</Form.Label>
+                        <Form.Control required type="password" id="inputpassword" name="password" onChange={readUserPasswordEdit} />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" >
+                        Enviar
                 </Button>
                 </Form>
             </>
