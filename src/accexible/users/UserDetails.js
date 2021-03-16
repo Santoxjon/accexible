ee144ca86849ba0c9a2ba69c73a3359119b9aecb
Santoxjon <jonsantox@gmail.com>
2021-03-16 23:54:13 +0100

    Detalles del usuario / actualizar

--- src/accexible/users/UserDetails.js
+++ src/accexible/users/UserDetails.js
@@ -1,119 +1,92 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import React, { useState, useEffect } from 'react';
-import { getCookie, deleteCookie } from '../Functions';
+import { getCookie } from '../Functions';
 import { Redirect } from 'react-router-dom';
 
 function UserDetails() {
-
-    let [userCookie, setUserCookie] = useState({ userId: getCookie("userId"), loginToken: getCookie("loginToken") });
-    let [usernameEdit, setUsernameEdit] = useState("");
-    let [userEmailEdit, setUserEmailEdit] = useState("");
-    let [usernameChange, setUsernameChange] = useState("");
-    let [userEmailChange, setUserEmailChange] = useState("");
-    let [userIdEdit, setUserIdEdit] = useState("");
-    let [userPasswordEdit, setUserPasswordEdit] = useState("");
-    let [userPasswordChange, setUserPasswordChange] = useState("");
-    
-function readUsernameEdit(event) {
-    setUsernameChange(event.target.value)
-}
-
-function readUserEmailEdit(event) {
-    setUserEmailChange(event.target.value)
-}
-
-function readUserPasswordEdit(event) {
-    setUserPasswordChange(event.target.value)
-}
-
-function editUser(e) {
-    e.preventDefault();
-    console.log(usernameChange, userEmailChange)
-    var editOldUser = {
-        name: usernameChange,
-        email: userEmailChange,
-        password: userPasswordChange,
-        id: userIdEdit
-    }
-
-    var fetchOldUser = {
-        method: 'PUT',
-        body: JSON.stringify(editOldUser),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8'
-        }
-    }
-
-    fetch(`http://localhost:9000/users/updateuser?userId=${userCookie.userId}&loginToken=${userCookie.loginToken}`, fetchOldUser)
-    .then(respuesta => respuesta.json())
-    .then((data => console.log(data)))
-}
+    const userCookie = { userId: getCookie("userId"), loginToken: getCookie("loginToken") };
+    const [newUsername, setNewUsername] = useState("");
+    const [newEmail, setNewEmail] = useState("");
+    const [oldEmail, setOldEmail] = useState("")
+    const [emailError, setEmailError] = useState("");
+    const btnStatus = emailError === "" ? false : true;
 
     useEffect(() => {
-        console.log(userCookie)
-        fetch(`http://localhost:9000/users/checkloggeduser?userId=${userCookie.userId}&loginToken=${userCookie.loginToken}`)
+        fetch(`http://localhost:9000/users/checkToken?id=${userCookie.userId}&token=${userCookie.loginToken}`)
             .then(res => res.json())
             .then(res => {
-                setUsernameEdit(res.name)
-                setUserEmailEdit(res.email)
-                setUserIdEdit(res._id)
-                setUserPasswordEdit(res.password)
-            }
-            )
-    }, [])
+                setNewUsername(res.name);
+                setOldEmail(res.email);
+                setNewEmail(res.email);
+            });
+    }, []);
 
     useEffect(() => {
+        checkEmail();
+    }, [newEmail])
 
-        let regExNumber = new RegExp(/[0-9]/, 'g')  //must contain one digit from 0-9
-        let regExCapital = new RegExp(/[A-Z]/, 'g') //must contain 1 character from A-Z
+    function updateUser(e) {
+        e.preventDefault();
+        var editOldUser = {
+            name: newUsername,
+            email: newEmail,
+            id: userCookie.userId
+        }
 
-        if ((userPasswordEdit.length > 7 && userPasswordEdit.length < 21) && regExNumber.test(userPasswordEdit) && regExCapital.test(userPasswordEdit)) {
-            
+        var fetchOldUser = {
+            method: 'PUT',
+            body: JSON.stringify(editOldUser),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            }
+        }
+
+        fetch(`http://localhost:9000/users/updateUser`, fetchOldUser)
+            .then(res => res.json())
+            .then(() => {
+                window.location = "/profile"
+            });
+    }
+
+    function checkEmail(e) {
+        if (oldEmail === newEmail) {
+            setEmailError("");
         }
         else {
-           
-            if (!(userPasswordEdit.length > 7 && userPasswordEdit.length < 21)) {
-                
-            }
-            else {
-                
-            }
-            if (!regExNumber.test(userPasswordEdit)) {
-              
-            }
-            else {
-              
-            }
-            if (!regExCapital.test(userPasswordEdit)) {
-               
-            }
-            else {
-                
-            }
+            fetch(`http://localhost:9000/users/checkEmail?email=${newEmail}`)
+                .then(res => res.json())
+                .then((user) => {
+                    if (user) {
+                        setEmailError("Email en uso")
+                    }
+                    else {
+                        setEmailError("")
+                    }
+                });
         }
-    }, [userPasswordEdit])
+    }
 
     if (userCookie.userId && userCookie.loginToken) {
 
         return (
             <>
-            <Form id="updateForm" onSubmit={editUser}>
-                <Form.Group>
-                    <Form.Label>Nombre y apellidos</Form.Label>
-                    <Form.Control type="text" placeholder={usernameEdit} id="inputname" name="name" onChange={readUsernameEdit}/>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder={userEmailEdit} id="inputemail" name="email" onChange={readUserEmailEdit} />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Contraseña</Form.Label>
-                    <Form.Control type="password" id="inputpassword" name="password" onChange={readUserPasswordEdit} />
-                </Form.Group>
-                <Button type="submit" >
-                    Enviar
-                </Button>
+                <Form id="updateForm" onSubmit={updateUser}>
+                    <h1>Detalles del perfil</h1>
+                    <Form.Group>
+                        <Form.Label>Nombre y apellidos</Form.Label>
+                        <Form.Control type="text" value={newUsername} id="inputname" name="name" onChange={(e) => setNewUsername(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" value={newEmail} id="inputemail" name="email" onChange={(e) => setNewEmail(e.target.value)} />
+                        <Form.Text className="text-alert" >
+                            {emailError}
+                        </Form.Text>
+                    </Form.Group>
+                    <Button type="submit" disabled={btnStatus}>
+                        Actualizar
+                    </Button>
                 </Form>
             </>
         )
