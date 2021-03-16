6db8870bd775e54c92a8b7d61d120db2bcf94a00
Javi <inbox.javiergm@gmail.com>
2021-03-16 11:55:35 +0100

    Creación archivo para editar User

---
+++ src/accexible/users/UserDetails.js
@@ -0,0 +1,13 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
+
+function UserDetails() {
+return(
+    <>
+    <p>Holi</p>
+    </>
+)
+}
+
+export default UserDetails;
