1b0f7e043ab54983adbf616b163041e54a219d3e
Santoxjon <jonsantox@gmail.com>
2021-03-17 15:18:43 +0100

    Mesaje alerta al hacer cambios en el usuario

--- src/accexible/users/UserDetails.js
+++ src/accexible/users/UserDetails.js
@@ -1,15 +1,22 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 import React, { useState, useEffect } from 'react';
 import { getCookie } from '../Functions';
 import { Redirect } from 'react-router-dom';
 
 function UserDetails() {
     const userCookie = { userId: getCookie("userId"), loginToken: getCookie("loginToken") };
-    const [newUsername, setNewUsername] = useState("");
-    const [newEmail, setNewEmail] = useState("");
+    const [newUsername, setNewUsername] = useState(undefined);
+    const [newEmail, setNewEmail] = useState(undefined);
     const [oldEmail, setOldEmail] = useState("")
+
     const [emailError, setEmailError] = useState("");
+
+    const [updateMessage, setUpdateMessage] = useState("");
+    const [alertVisibility, setAlertVisibility] = useState(true);
+
+    const inputStatus = !(newUsername && newEmail);
     const btnStatus = emailError === "" ? false : true;
 
     useEffect(() => {
@@ -45,7 +52,8 @@ function UserDetails() {
         fetch(`http://localhost:9000/users/updateUser`, fetchOldUser)
             .then(res => res.json())
             .then(() => {
-                window.location = "/profile"
+                setAlertVisibility(true);
+                setUpdateMessage("Datos actualizados con éxito!")
             });
     }
 
@@ -67,19 +75,32 @@ function UserDetails() {
         }
     }
 
-    if (userCookie.userId && userCookie.loginToken) {
+    function AlertMessage() {
+        if (updateMessage !== "") {
+            if (alertVisibility) {
+                return (
+                    <Alert variant="success" onClose={() => setAlertVisibility(false)} dismissible>
+                        Datos actualizados con éxito!
+                    </Alert>
+                );
+            }
+        }
+        return (<></>)
+    }
 
+    if (userCookie.userId && userCookie.loginToken) {
         return (
             <>
                 <Form id="updateForm" onSubmit={updateUser}>
                     <h1>Detalles del perfil</h1>
+                    <AlertMessage />
                     <Form.Group>
                         <Form.Label>Nombre y apellidos</Form.Label>
-                        <Form.Control type="text" value={newUsername} id="inputname" name="name" onChange={(e) => setNewUsername(e.target.value)} />
+                        <Form.Control disabled={inputStatus} type="text" value={newUsername} id="inputname" name="name" onChange={(e) => setNewUsername(e.target.value)} />
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" value={newEmail} id="inputemail" name="email" onChange={(e) => setNewEmail(e.target.value)} />
+                        <Form.Control disabled={inputStatus} type="email" value={newEmail} id="inputemail" name="email" onChange={(e) => setNewEmail(e.target.value)} />
                         <Form.Text className="text-alert" >
                             {emailError}
                         </Form.Text>
