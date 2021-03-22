191beab30e2cf8c948a3123574cd15d7eb33cc9b
Ssergiomc <sergiomc84@live.com>
2021-03-22 01:00:06 +0100

    act necesidad conexion login + estructura return

--- src/accexible/users/Results.js
+++ src/accexible/users/Results.js
@@ -1,17 +1,38 @@
+import { useEffect, useState } from "react";
+import { getCookie } from '../Functions';
+import { Redirect } from 'react-router-dom';
+
 import ResultTest from './ResultTest';
 import ResultChatbot from './ResultChatbot';
 
 
-function Results (){
+function Results() {
+    const userCookie = { userId: getCookie("userId"), loginToken: getCookie("loginToken") };
+    const [username, setUsername] = useState("")
 
+    useEffect(() => {
+        fetch(`http://localhost:9000/users/checkToken?id=${userCookie.userId}&token=${userCookie.loginToken}`)
+            .then(res => res.json())
+            .then(res => {
+                setUsername(res.name);
+            });
+    }, []);
 
-    return (
-        <>
-            <h2>Resultados</h2>
-            <ResultTest/>
-            <ResultChatbot/>
+    if (userCookie.userId && userCookie.loginToken) {
 
-        </>
-    )
+        return (
+            <div id="resultsContainer">
+                <h1>Resultados {username}</h1>
+
+                <ResultTest />
+                <ResultChatbot />
+
+            </div>
+        )
+    } else {
+        return (
+            <Redirect to="/login" />
+        )
+    }
 }
 export default Results;
