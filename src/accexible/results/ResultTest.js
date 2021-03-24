8c247fc5ff53247ea9399d394be596237b0cdd1f
Ssergiomc <sergiomc84@live.com>
2021-03-24 16:55:04 +0100

    get resultados test y envio por props a cada comp

--- src/accexible/results/ResultTest.js
+++ src/accexible/results/ResultTest.js
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
+import { getCookie } from '../Functions';
 import Form from 'react-bootstrap/Form';
-
+import { API_URL } from '../Consts';
+import ResultTestTable from './ResultTestTable';
 import ResultTestLine from './ResultTestLine';
 import ResultTestDonut from './ResultTestDonut';
 
 
 function ResultTest() {
+    const userCookie = { userId: getCookie("userId"), loginToken: getCookie("loginToken") };
+    const [userTestResults, setUserTestResults] = useState([]);
+
+
+    /* GET ALL RESULTS FROM THE USER */
+
+    useEffect(() => {
+        fetch(`${API_URL}/test/resultsuser/${userCookie.userId}`)
+            .then(res => res.json())
+            .then(allTestResults => {
+                setUserTestResults(allTestResults);
+            });
+
+
+    }, []);
+
+    useEffect(() => {
+
+        console.log(userTestResults);
+        // <ResultTest />
+    }, [userTestResults]);
 
 
     return (
@@ -14,10 +38,11 @@ function ResultTest() {
                 <p>Explicacion gral acorde a los resultados 4 posibles resultados? con diferentes orientaciones sobre como seguir</p>
                 <p>El resultado es obtenido a través de la valoración del test de preguntas cerradas realizado anteriormente. Tras su estudio por medio de un algoritmo, se le ofrece al usuario unos resultados e indicaciones personalizadas, correspondientes con el análisis realizado a través de las respuestas señaladas. </p>
                 {/* meter tabla con los resultados obtenidos del ultimo test */}
+                <ResultTestTable tableTestRes={userTestResults}/>
             </div>
 
             <div id="graphicsTestResults">
-                <ResultTestLine />
+                <ResultTestLine userTestRes={userTestResults}/>
                 <ResultTestDonut />
             </div>
 
