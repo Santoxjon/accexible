140cd1e0d3d289b54b72e639811d89e66ee41420
Ssergiomc <sergiomc84@live.com>
2021-03-29 09:14:12 +0200

    act tabla dinamica muestra resultados opciones

--- src/accexible/results/ResultTestTable.js
+++ src/accexible/results/ResultTestTable.js
@@ -4,53 +4,46 @@ import Table from 'react-bootstrap/Table';
 
 function ResultTestTable(props) {
 
-    let [arrayAnswers, setArrayAnswers] = useState(props.tableTestRes);
-    let lastResult = props.tableTestRes;
-    // let arrayAnswers = lastResult[0];
-    // let arrayAllAnswers = arrayAnswers.answers;
+    let data = props.showTable;
+    let [arrayData, setArrayData] = useState([]);
+    let [arrayTableQuestShow, setArrayTableQuestShow] = useState([]);
+    let [arrayTableAnswShow, setArrayTableAnswShow] = useState([]);
 
     useEffect(() => {
-        console.log(arrayAnswers);
+        // console.log(data);
+        setArrayData(data.answers);
+    }, [data]);
 
-        // console.log("*****");
-        // console.log(arrayAllAnswers);
-        // console.log("*****");
-
-        // console.log(arrayAnswers.answers);
-        // console.log(arrayAnswers.answers[0]);
-    }, []);
-
-
-    // let showTable = arrayAnswers.map((answer, index) => {
-    //     return (
-    //         <td key={index}>{answer ? answer : "Cargando resultados"}</td>
-    //     )
-    // })
+    useEffect(() => {
+        // console.log(arrayData);
+        if (arrayData !== "" && arrayData !== undefined) {
+            setArrayTableQuestShow(arrayData.map(function (test, index) {
+                return (
+                    <td key={index}>{index + 1}</td>
+                )
+            }))
+            setArrayTableAnswShow(arrayData.map(function (test, index) {
+                return (
+                    <td key={index}>{test}</td>
+                )
+            }))
+        }
+    }, [arrayData]);
 
 
     return (
         <div>
-            <p>Respuestas Tabla (borrar): {lastResult[0] ? lastResult[0].answers : "Cargando resultados"}</p>
             <Table striped bordered hover size="sm">
                 <thead>
                     <tr>
-                        <th >#Pregunta</th>
-                        <th>1</th>
-                        <th>2</th>
-                        <th>3</th>
-                        <th>4</th>
-                        <th>5</th>
-                        <th>6</th>
-                        <th>7</th>
-                        <th>8</th>
-                        <th>9</th>
+                        <th>Nº Pregunta</th>
+                        {arrayTableQuestShow}
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td>Respuesta</td>
-                        {/* <td>{showTable ? showTable : "Cargando resultados"}</td> */}
-
+                        {arrayTableAnswShow}
                     </tr>
                 </tbody>
             </Table>
